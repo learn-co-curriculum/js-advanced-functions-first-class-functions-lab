534b121ded9ede8c9c766f0e57fa4c57af807aaf
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-22 17:14:44 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+let drivers;
+function returnFirstTwoDrivers(drivers){
+    const newDrivers = drivers.slice(0,2);
+    return newDrivers;
+}
+
+function returnLastTwoDrivers(drivers){
+    const newDrivers = drivers.slice(2);
+    return newDrivers;
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers ,
+    returnLastTwoDrivers
+    ];
+
+function createFareMultiplier(multiplier){
+    return function(){return multiplier * multiplier};
+}
+
+function fareDoubler(fare){
+    return fare * 2;
+}
+
+function fareTripler(fare){
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, select){
+    const newDrivers = select(drivers);
+    return newDrivers
+}
